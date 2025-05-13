20a1e193543e1805efb3c9d9b4ed0ca9e60952e9
Huch-gif <savvabluz2@gmail.com>
2025-05-13 11:05:16 +0300

    add new class

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ class Warrior ():
     def eat(self):
         print(f"{self.name} сел кушать")
 
-         self.power = + 1
+        self.power = + 1
 
 def hit(self):
          print(f"{self.name} бьет кого-то")
-          self.endurance =- 6
+         self.endurance =- 6
 
 
 def info(self):
@@ -30,3 +30,6 @@ def info(self):
          print(f"цвет волос воина - {self.hair_color}")
          print(f"сила воина - {self.power}")
          print(f"выносливость воина - {self.endurance}")
+
+
+
